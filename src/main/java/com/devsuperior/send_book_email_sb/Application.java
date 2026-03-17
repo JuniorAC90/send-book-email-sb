8466fe4c84c3cc71f92fa6dfa69f481e12afa1fa
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-03-17 16:16:38 -0300

    Project created

---
+++ src/main/java/com/devsuperior/send_book_email_sb/Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.send_book_email_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
